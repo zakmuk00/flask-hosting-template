85034f57fc80d7bb7353fb3e1be1a799036419b7
zakmuk00 <zakariyamukh@gmail.com>
2026-07-01 13:19:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,18 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
